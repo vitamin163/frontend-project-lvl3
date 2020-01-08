e5519a882277c7186c3d4ce94eaed28c7d27544f
Vitaliy D <vitalik.domnin@gmail.com>
2020-01-08 13:30:23 +0400

    refactoring

--- src/app/parser.js
+++ src/app/parser.js
@@ -2,18 +2,30 @@ export default rss => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(rss, 'application/xml');
 
-  const title = doc.querySelector('title').textContent;
+  const titleElement = doc.querySelector('title');
+  const title = titleElement.textContent;
 
-  const description = doc.querySelector('description').textContent;
-  const feedLink = doc.querySelector('link').textContent;
+  const descriptionElement = doc.querySelector('description');
+  const description = descriptionElement.textContent;
+
+  const linkElement = doc.querySelector('link');
+  const feedLink = linkElement.textContent;
+
   const feed = { title, description, feedLink };
 
   const items = [...doc.querySelectorAll('item')];
   const posts = items.map(item => {
-    const titleItem = item.querySelector('title').textContent;
-    const descriptionItem = item.querySelector('description').textContent;
-    const pubDate = Date.parse(item.querySelector('pubDate').textContent);
-    const postLink = item.querySelector('link').textContent;
+    const titleItemElement = item.querySelector('title');
+    const titleItem = titleItemElement.textContent;
+
+    const descriptionItemElement = item.querySelector('description');
+    const descriptionItem = descriptionItemElement.textContent;
+
+    const pubDateElement = item.querySelector('pubDate');
+    const pubDate = Date.parse(pubDateElement.textContent);
+
+    const postLinkElement = item.querySelector('link');
+    const postLink = postLinkElement.textContent;
     return { titleItem, descriptionItem, pubDate, feedLink, postLink };
   });
 
